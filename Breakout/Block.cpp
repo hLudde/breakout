f6dcb5cf8d7b9afa4aa15b25944df2c9cbc6de40
Erlend Dullaert <erlenddullaert@gmail.com>
2019-04-02 16:31:06 +0200

    Block and Vector2 initial

---
+++ Breakout/Block.cpp
@@ -0,0 +1,19 @@
+﻿#include "Block.h"
+
+Block::Block(Vector2 pos): pos(pos), height(50), width(100), rect() {
+	rect.x = pos.x;
+	rect.y = pos.y;
+	rect.h = height;
+	rect.w = width;
+	surface = SDL_CreateRGBSurface(0, width, height, 0, 0, 0, 0, 0);
+	SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 0, 0));
+}
+
+Block::Block(Vector2 pos, int h, int w, Uint32 color): pos(pos), height(h), width(w), rect() {
+	rect.x = pos.x;
+	rect.y = pos.y;
+	rect.h = height;
+	rect.w = width;
+	surface = SDL_CreateRGBSurface(0, width, height, 0, 0, 0, 0, 0);
+	SDL_FillRect(surface, &rect, color);
+}
---
+++ Breakout/Vector2.cpp
@@ -0,0 +1,19 @@
+﻿#include "Vector2.h"
+#include <complex>
+
+float Vector2::Length() const {
+	return std::sqrt(x * x + y * y);
+}
+
+Vector2 Vector2::Normalize() {
+	const float len = Length();
+	x = x / len;
+	y = y / len;
+	return *this;
+}
+
+Vector2 Vector2::Negate() {
+	x = -x;
+	y = -y;
+	return *this;
+}
