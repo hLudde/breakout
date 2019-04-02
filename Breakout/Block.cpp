62896f74080ab382db78b3542fabe731946a8493
Erlend Dullaert <erlenddullaert@gmail.com>
2019-04-02 17:03:16 +0200

    made variables in constructor of Block const

--- Breakout/Block.cpp
+++ Breakout/Block.cpp
@@ -1,6 +1,6 @@
 ﻿#include "Block.h"
 
-Block::Block(Vector2 pos): pos(pos), height(50), width(100), rect() {
+Block::Block(const Vector2 pos): pos(pos), height(50), width(100), rect() {
 	rect.x = static_cast<int>(pos.x);
 	rect.y = static_cast<int>(pos.y);
 	rect.h = height;
@@ -9,7 +9,7 @@ Block::Block(Vector2 pos): pos(pos), height(50), width(100), rect() {
 	SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 0, 0));
 }
 
-Block::Block(Vector2 pos, int h, int w, Uint8 r, Uint8 g, Uint8 b): pos(pos), height(h), width(w), rect() {
+Block::Block(const Vector2 pos, const int h, const int w, const Uint8 r, const Uint8 g, const Uint8 b): pos(pos), height(h), width(w), rect() {
 	rect.x = static_cast<int>(pos.x);
 	rect.y = static_cast<int>(pos.y);
 	rect.h = height;
