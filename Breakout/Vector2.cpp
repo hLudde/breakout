5705e9d7aef9ef76c2165b7349b0fd23738c28f5
Erlend Dullaert <erlenddullaert@gmail.com>
2019-04-04 14:43:19 +0200

    added operator overloads; +-*/

--- Breakout/Vector2.cpp
+++ Breakout/Vector2.cpp
@@ -5,15 +5,53 @@ float Vector2::Length() const {
 	return std::sqrt(x * x + y * y);
 }
 
-Vector2 Vector2::Normalize() {
-	const float len = Length();
-	x = x / len;
-	y = y / len;
-	return *this;
+Vector2 Vector2::operator+(const Vector2 other) const
+{
+	Vector2 result;
+
+	result.x = x + other.x;
+	result.y = y + other.y;
+
+	return result;
 }
 
-Vector2 Vector2::Negate() {
-	x = -x;
-	y = -y;
-	return *this;
+Vector2 Vector2::operator-(const Vector2 other) const
+{
+	Vector2 result;
+
+	result.x = x - other.x;
+	result.y = y - other.y;
+
+	return result;
+}
+
+Vector2 Vector2::operator*(const float scalar) const
+{
+	Vector2 result;
+
+	result.x = x * scalar;
+	result.y = y * scalar;
+
+	return result;
+}
+
+Vector2 Vector2::operator/(const float divisor) const
+{
+	Vector2 result;
+
+	result.x = x / divisor;
+	result.y = y / divisor;
+
+	return result;
+}
+
+Vector2 Vector2::Normalize() const
+{
+	return *this / Length();
+}
+
+Vector2 Vector2::Negate() const
+{
+	return *this * -1;
+
 }
--- Breakout/Vector2.h
+++ Breakout/Vector2.h
@@ -3,12 +3,16 @@
 class Vector2 {
 public:
 	explicit Vector2(): x(0), y(0) {}
-	explicit Vector2(const float x, const float y): x(x), y(y) {
-	}
+	explicit Vector2(const float x, const float y): x(x), y(y) {}
 
 	float x, y;
 
-	Vector2 Normalize();
-	Vector2 Negate();
+	Vector2 Normalize() const;
+	Vector2 Negate() const;
 	float Length() const;
+
+	Vector2 operator+ (Vector2 other) const;
+	Vector2 operator- (Vector2 other) const;
+	Vector2 operator* (float scalar) const;
+	Vector2 operator/ (float divisor) const;
 };
