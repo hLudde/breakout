c0a9fa4c1a6a2a8c20694710c2a0ada986f3ca67
Erlend Dullaert <erlenddullaert@gmail.com>
2019-04-04 14:50:16 +0200

    added CreateTexture and made the constructors require a SDL_Renderer*

--- Breakout/Block.cpp
+++ Breakout/Block.cpp
@@ -1,27 +1,24 @@
 ﻿#include "Block.h"
 
-Block::Block() {
-	pos = Vector2(0.0f, 0.0f);
-	height = 50;
-	width = 100;
-
-	rect.h = height;
-	rect.w = width;
-
-	surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-	SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 0, 0));
-}
-
-Block::Block(const Vector2 pos): pos(pos), height(50), width(100) {
+Block::Block(const Vector2 pos, SDL_Renderer* renderer): pos(pos), height(50), width(100) {
 	rect.h = height;
 	rect.w = width;
 	surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
 	SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 0, 0));
+
+	CreateTexture(renderer);
 }
 
-Block::Block(const Vector2 pos, const int h, const int w, const Uint8 r, const Uint8 g, const Uint8 b): pos(pos), height(h), width(w) {
+Block::Block(const Vector2 pos, const int h, const int w, const Uint8 r, const Uint8 g, const Uint8 b, SDL_Renderer* renderer): pos(pos), height(h), width(w) {
 	rect.h = height;
 	rect.w = width;
 	surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
 	SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format,r,g,b));
+
+	CreateTexture(renderer);
+}
+
+void Block::CreateTexture(SDL_Renderer* renderer)
+{
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
 }
--- Breakout/Block.h
+++ Breakout/Block.h
@@ -4,20 +4,27 @@
 
 class Block {
 public:
-	explicit Block();
-	explicit Block(Vector2 pos);
+	explicit Block() = default;
+	explicit Block(Vector2 pos, SDL_Renderer* renderer);
 
-	explicit Block(Vector2 pos, int h, int w, Uint8 r, Uint8 g, Uint8 b);
+	explicit Block(Vector2 pos, int h, int w, Uint8 r, Uint8 g, Uint8 b, SDL_Renderer* renderer);
 
+	/**
+	 *Should not be used. use GetTexture() instead.
+	 *When this is not used, should call SDL_FreeSurface() in CreateTexture()
+	 */
 	SDL_Surface* GetSurface() const { return surface; }
+	SDL_Texture* GetTexture() const { return texture; }
 	Vector2* GetPos() { return &pos; }
 	int GetHeight() const { return height; }
 	int GetWidth() const { return width; }
+
+	void CreateTexture(SDL_Renderer* renderer);
 
 private:
 	Vector2 pos;
 	SDL_Rect rect{};
 	int height, width;
 	SDL_Surface* surface;
-
+	SDL_Texture* texture = nullptr;
 };
