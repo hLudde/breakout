2cdcc226f453f48ca2066bab62dcce0bd417c274
Erlend Dullaert <erlenddullaert@gmail.com>
2019-04-04 14:51:06 +0200

    Added BrickLayer class. This class is responsible for creating the map

---
+++ Breakout/BrickLayer.cpp
@@ -0,0 +1,19 @@
+﻿#include "BrickLayer.h"
+#include "Block.h"
+
+void BrickLayer::CreateMap(const int windowWidth, const int windowHeight, SDL_Renderer* renderer)
+{
+	const int INIT_OFFSET_H = 100;
+	const int INIT_OFFSET_W = 0;
+
+	const int bWidth = windowWidth/10;		//Width of the blocks
+	const int bHeight = windowHeight/3/4;	//Height of the blocks
+
+	for (int i=0;i<10;i++)
+		for(int j=0;j<4;j++)
+		{
+			Vector2 pos{ float(INIT_OFFSET_W + i * bWidth),float(INIT_OFFSET_H + j * bHeight) };
+			Block b{ pos,bHeight-2,bWidth-2,Uint8(55 + rand() % 200),Uint8(55 + rand() % 200),Uint8(55 + rand() % 200),renderer };
+			map.push_back(b);
+		}
+}
---
+++ Breakout/BrickLayer.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Block.h"
+#include <vector>
+
+class BrickLayer
+{
+public:
+	BrickLayer() :map(std::vector<Block>()) {  }
+
+	void CreateMap(int windowWidth, int windowHeight, SDL_Renderer* renderer);
+
+	std::vector<Block>* GetMap() { return &map; }
+private:
+	std::vector<Block> map;
+};
