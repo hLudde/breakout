f932acd215166257f285622a076514c2ce75e4e6
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-04-09 15:45:36 +0200

    Cleaned up breakout and the gameloop to use renderer

--- Breakout/Breakout.cpp
+++ Breakout/Breakout.cpp
@@ -22,7 +22,6 @@ const int SCREEN_WIDTH = 750+50;
 bool Init(SDL_Window* &window, SDL_Surface* &screenSurface);
 void Close(SDL_Window* &window);
 int LoadAndDisplayImage(SDL_Window* &window, SDL_Surface* &screenSurface);
-void RenderMap(SDL_Renderer* renderer, std::vector<Block>* map);
 
 /*main*/
 int main(int argc, char* argv[]) {
@@ -59,7 +58,6 @@ bool Init(SDL_Window* &window, SDL_Surface* &screenSurface) {
 		else {
 			screenSurface = SDL_GetWindowSurface(window);
 		}
-		//SDL_SetWindowResizable(window, SDL_TRUE);
 	}
 	return success;
 }
@@ -70,48 +68,21 @@ int LoadAndDisplayImage(SDL_Window* &window, SDL_Surface* &screenSurface) {
 	Uint32 fps_lasttime = SDL_GetTicks(); //the last recorded time.
 	Uint32 fps_frames = 0; //frames passed since the last recorded fps.
 
-	//Renderer& renderer = Renderer::Init(window);
-
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	
-	/*if (renderer == nullptr) {
-		std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
-	a	Close(window);
-		return EXIT_FAILURE;
-	}*/
-	Player player = Player(Vector2((SCREEN_WIDTH-PLAYER_WIDTH)/2, SCREEN_HEIGHT-150), PLAYER_HEIGHT, PLAYER_WIDTH, 0x12, 0xF2, 0x5F,renderer, screenSurface->w);
-	SDL_Surface* surface = player.GetSurface();//Block(Vector2(0 , SCREEN_HEIGHT - 150), PLAYER_HEIGHT, PLAYER_WIDTH, 255, 0,0).GetSurface();
-	if (surface == nullptr) {
-		std::cerr << "Failed to create surface: " << SDL_GetError() << std::endl;
-		Close(window);
-		return EXIT_FAILURE;
-	}
-
-	SDL_Texture* drawable = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_Rect coords;
-
-	coords.h = surface->h;
-	coords.w = surface->w;
-	coords.x = 0;
-	coords.y = 0;
-	float y = SCREEN_HEIGHT-150;
-	Timer& timer = Timer::GetInstance();
-
-	SDL_FreeSurface(surface);
-	/*renderer init STOP*/
+	Renderer& renderer = Renderer::Init(window);
+	Player player = Player(Vector2((SCREEN_WIDTH-PLAYER_WIDTH)/2, SCREEN_HEIGHT-150), PLAYER_HEIGHT, PLAYER_WIDTH, 0x12, 0xF2, 0x5F, screenSurface->w);
+	InputManager& inputManager = InputManager::GetInstance();
+	Timer&  timer = Timer::GetInstance();
+	///*renderer init STOP*/
 
 	BrickLayer BL;
-	BL.CreateMap(SCREEN_WIDTH, SCREEN_HEIGHT,renderer);
-	std::vector<Block>* map = BL.GetMap();
+	BL.CreateMap(SCREEN_WIDTH, SCREEN_HEIGHT);
+	std::vector<Block*>* map = BL.GetMap();
 
-	Ball ball{ Vector2{static_cast<int>(SCREEN_WIDTH / 2),500.0f},Vector2{1.0f,0.5f}, 10.0f,127,127,127,renderer };
-
-	InputManager& inputManager = InputManager::GetInstance();
+	Ball ball{ Vector2{static_cast<int>(SCREEN_WIDTH / 2),500.0f},Vector2{1.0f,0.5f}, 10.0f,127,127,127 };
 
 	/*GameLoop*/
 	while (true) {
 		/*Input manager*/
-
 		timer.UpdateDeltaTime();
 		inputManager.Update();
 		player.MovePlayer();
@@ -121,22 +92,9 @@ int LoadAndDisplayImage(SDL_Window* &window, SDL_Surface* &screenSurface) {
 		}
 		/*if (ball.IsDead())
 			Close(window);*/
-
-		/*renderer*/
-
-		RenderMap(renderer,map);
-
-		ball.CheckCollision(SCREEN_HEIGHT,SCREEN_WIDTH,map,&player.GetBlock());
+		ball.CheckCollision(SCREEN_HEIGHT,SCREEN_WIDTH,map,player.GetBlock());
 		ball.MoveBall();
-		SDL_RenderCopy(renderer, ball.GetTexture(), nullptr, ball.GetRect());
-
-		coords.x = static_cast<int>(player.GetPos());
-		coords.y = static_cast<int>(y);
-		SDL_RenderCopy(renderer, drawable, nullptr, &coords);
-		SDL_RenderPresent(renderer);
-		SDL_RenderClear(renderer);
-
-
+		renderer.Render();
 		fps_frames++;
 		if (fps_lasttime < SDL_GetTicks() - FPS_INTERVAL * 1000)
 		{
@@ -144,7 +102,6 @@ int LoadAndDisplayImage(SDL_Window* &window, SDL_Surface* &screenSurface) {
 			printf("FPS: %d\n", fps_frames);
 			fps_frames = 0;
 		}
-
 	}
 	return EXIT_SUCCESS;
 }
@@ -154,19 +111,4 @@ void Close(SDL_Window* &window) {
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return;
-}
-
-void RenderMap(struct SDL_Renderer* renderer, std::vector<Block>* map)
-{
-
-	for (int i=0;i < map->size();i++)
-	{
-		Block& block = map->at(i);
-
-		if (block.GetTexture() == nullptr)
-			block.CreateTexture(renderer);
-
-		SDL_Rect rect { int(block.GetPos()->x),int(block.GetPos()->y),block.GetWidth(),block.GetHeight() };
-		SDL_RenderCopy(renderer, block.GetTexture(), nullptr, &rect);
-	}
 }
